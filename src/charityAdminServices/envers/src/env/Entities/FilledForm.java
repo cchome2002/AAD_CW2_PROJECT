b5867304cddcf3cc0d5f524ebfe6b6414ed8e947
Christos Tolakis <ctolakis@yahoo.com>
2013-01-26 22:07:45 +0000

    Extended #2
    WIP

--- src/charityAdminServices/envers/src/env/Entities/FilledForm.java
+++ src/charityAdminServices/envers/src/env/Entities/FilledForm.java
@@ -1,6 +1,7 @@
 package env.Entities;
 
 import java.sql.Timestamp;
+import java.util.Set;
 
 import javax.persistence.Entity;
 
@@ -10,22 +11,31 @@ import org.hibernate.envers.Audited;
 @Audited
 public class FilledForm {
 	
-	private Integer form_id;
-	private String column_id;
+	private Integer filled_form_id;
+	private Form form_id;
+	private Set<FormFields> field_id;
 	private String value;
-	private Integer user_id;
+	private User user_id;
+	private Integer record_id;
 	private Boolean isActive;
-	public Integer getForm_id() {
+	private Timestamp timestamp;
+	public Integer getFilled_form_id() {
+		return filled_form_id;
+	}
+	public void setFilled_form_id(Integer filled_form_id) {
+		this.filled_form_id = filled_form_id;
+	}
+	public Form getForm_id() {
 		return form_id;
 	}
-	public void setForm_id(Integer form_id) {
+	public void setForm_id(Form form_id) {
 		this.form_id = form_id;
 	}
-	public String getColumn_id() {
-		return column_id;
+	public Set<FormFields> getField_id() {
+		return field_id;
 	}
-	public void setColumn_id(String column_id) {
-		this.column_id = column_id;
+	public void setField_id(Set<FormFields> field_id) {
+		this.field_id = field_id;
 	}
 	public String getValue() {
 		return value;
@@ -33,11 +43,17 @@ public class FilledForm {
 	public void setValue(String value) {
 		this.value = value;
 	}
-	public Integer getUser_id() {
+	public User getUser_id() {
 		return user_id;
 	}
-	public void setUser_id(Integer user_id) {
+	public void setUser_id(User user_id) {
 		this.user_id = user_id;
+	}
+	public Integer getRecord_id() {
+		return record_id;
+	}
+	public void setRecord_id(Integer record_id) {
+		this.record_id = record_id;
 	}
 	public Boolean getIsActive() {
 		return isActive;
@@ -45,5 +61,12 @@ public class FilledForm {
 	public void setIsActive(Boolean isActive) {
 		this.isActive = isActive;
 	}
-
+	public Timestamp getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(Timestamp timestamp) {
+		this.timestamp = timestamp;
+	}
+	
+	
 }
